855b17aa292df4aff739c2253605e6f5dd9a2651
Aditya Kresna <slaveofcode@gmail.com>
2022-09-09 21:33:15 +0700

    Check alias already exist

--- repository/pg/models/user.go
+++ repository/pg/models/user.go
@@ -7,7 +7,7 @@ const UserTableName = "Users"
 type User struct {
 	BaseModel
 	Name     string         `gorm:"column:name;index" json:"name"`
-	Alias    string         `gorm:"column:alias;index" json:"alias"`
+	Alias    string         `gorm:"column:alias;uniqueIndex:idx_user_alias" json:"alias"`
 	Metadata datatypes.JSON `gorm:"column:metadata;index" json:"metadata"`
 }
 
